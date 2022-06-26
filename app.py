e8264e36fb69cdb6930ae64caf98b21ab70587b1
Python The Hack <lrulvoni@gmail.com>
2022-06-26 12:25:03 +0200

    update

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title("App")
+root.title("App One")
 root.geometry("500x500")
 root.resizable(False, False)
 root.config(bg='#232323')
 
+nome = 'pippo'
+cognome = 'pippone'
 #funzioni
-def mostra_messaggio():
-    print("clicck")
+def mostra_messaggio(nome):    
+    print("clicck" + nome)
+
+#struttura 
 
 label_titolo = tk.Label(root, text="titolo", font=("Microsoft YaHei", 16), bg="#232323", fg="white")
 label_titolo.grid(row=1, column=0, pady=20, padx=50 )
 
-
 label_subtitolo = tk.Label(root, text="SubTitolo", font=("Microsoft YaHei", 16), bg="#232323", fg="white")
 label_subtitolo.grid(row=2, column=0, pady=0, padx=50 )
 
-#bottoni
-button_click = tk.Button(root, text="click", font=("Times New Romans", 12), bg="#eb8034", fg="black", bd=3, width=15)
+#input 
+input_testo = tk.Entry(root, font=("Microsoft YaHei", 10), bd=3, width=100)
+input_testo.grid(row=4, column=0, pady=10)
+
+#area bottoni
+button_click = tk.Button(root, text="click", font=("Times New Romans", 12), bg="#eb8034", fg="black", bd=3, width=15, command=mostra_messaggio(f"{nome}"))
 button_click.grid(row=3, column=0, pady=10)
 
 
--- database.py
+++ database.py
@@ -1,18 +1,96 @@
 import sqlite3
+import datetime 
+import os
 
 
-try:
-    conn = sqlite3.connect("database.db")
+def check_db_exist():
+    database = 'database.db'
+    percorso = os.getcwd()
+    lista = os.listdir(percorso)
+    print("files nella directories is '", percorso, "' :")
+    isExist = os.path.exists(database)
+   
+    if isExist == True:
+        print(isExist)
+    else:
+        crea_database()
+    #print(lista)
+    # for x in os.listdir(): 
+    #     list = []
+    #     list.append(x)
+    #     database_file = "database.db"
+    #     isExist = os.path.exists(database_file)
+    #     print(isExist)
+      
 
-    cursor = conn.cursor() 
-    print("database creato")
+def crea_database():
+    try:
+        conn = sqlite3.connect("database.db")
 
-except sqlite3.Error as error:
-    print("abbiamo riscontrato un errore", error)
-finally:
-    if conn:
-        conn.close()
-        print("connessione terminata")
+        cursor = conn.cursor() 
+        print("database creato")
+
+    except sqlite3.Error as error:
+        print("abbiamo riscontrato un errore", error)
+    finally:
+        if conn:
+            conn.close()
+            print("connessione terminata")
+
+    try:
+        conn = sqlite3.connect('database.db')
+        create_table_query = '''CREATE TABLE IF NOT EXISTS clienti(
+                            id INTEGER PRIMARY KEY autoincrement NOT NULL,
+                            nome TEXT NOT NULL,
+                            email TEXT NOT NULL UNIQUE,
+                            created_at datetime, 
+                            totale REAL NOT NULL);'''
+
+        cursor = conn.cursor() 
+        print("connessione al database")
+        cursor.execute(create_table_query)
+        conn.commit()
+        print("tabella creata")
+
+        cursor.close() 
+    except sqlite3.Error as error:
+        print("abbiamo un errore durante la creazione della tabella", error)
+    finally:
+        if conn:
+            conn.close() 
+            print("la connessione è terminata")
+
+#dati fake 
+nome = 'pippo'
+email = 'pippo@gmail.com'
+totale = 1000
+created_at = datetime.datetime.now()
 
 
+#funzione per inserire nuovi items nella tabella 
+def insert_into_db():
+    try:
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        print("connessione effettuata")
+
+        conn_insert_query = '''INSERT INTO clienti (nome, email, created_at, totale) VALUES (:nome,:email, :created_at, :totale)'''
+
+        count = cursor.execute(conn_insert_query)
+        conn.commit()
+        print("dati inseriti con successo", cursor.rowcount)
+        cursor.close() 
+
+    except sqlite3.Error as error:
+        print("non abbiamo potuto inserire dati nella tabella")
+        print(f'{nome} , {email} , {totale} , {created_at}')
+    finally:
+        if conn: 
+            conn.close()
+            print('la connessione al database è terminata con successo!')
+
+
+check_db_exist() 
+
+#insert_into_db() 
 
